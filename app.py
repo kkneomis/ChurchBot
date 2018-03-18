f746fba125e64f428521345a59ffef8d231c5e6c
Simeon Kakpovi <skakpovi@gmail.com>
2018-03-18 18:09:59 -0400

    testing timezone on heroku

--- app.py
+++ app.py
@@ -12,19 +12,23 @@ jarvis = Jarvis(app.logger)
 cron = Scheduler(daemon=True)
 # Explicitly kick off the background thread
 cron.start()
-
+now = str(datetime.now())
 print "Starting bot......"
+print "TIme now is", now
 
-@cron.cron_schedule(day_of_week='sun', hour=22 , minute=56)
+
+@cron.cron_schedule(day_of_week='sun', hour=18 , minute=11)
 def job_function():
     # This happens every Sunday at 9:30 am central time
     message = "Good morning class, here is the hangout link!"
-    link = "https://hangouts.google.com/hangouts/_/cfrz4i5iuzhfddbwoos23ji6xee"
+    print "Sending link....."
+    link = "Test, please ignore this"
+    #link = "https://hangouts.google.com/hangouts/_/cfrz4i5iuzhfddbwoos23ji6xee"
     jarvis.postLink(link)
     print "Link %s sent at %s" % (link, str(datetime.now()))
 
 
-@cron.cron_schedule(day='first mon', hour=1, minute=00)
+@cron.cron_schedule(day='1st mon', hour=1, minute=00)
 def job_function():
     # This happens on the last Sunday of every month at 7:00pm central time
     message = "Good evening class, please use this link to join the hangout at 7:00pm"
